d911154e5109cb3edbe0e2f4537e9f155ceabb09
vidhiPrajapati21 <prajapatividhi57@gmail.com>
2026-09-16 17:46:14 +0530

    Added Bhajan and Navratri to Event types

--- events/models.py
+++ events/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 
 class Event(models.Model):
 
     EVENT_TYPES = [
         ('Wedding', 'Wedding'),
-        ('Haldi','Haldi'),
-        ('school Function','school Function'),
+        ('Haldi', 'Haldi'),
+        ('school Function', 'school Function'),
         ('Birthday', 'Birthday'),
         ('Garba', 'Garba'),
+        ('Navratri', 'Navratri'),          
+        ('Bhajan', 'Bhajan'),              
         ('Corporate', 'Corporate'),
         ('DJ Night', 'DJ Night'),
-        ('Barat on wheels','Barat on wheels'),
+        ('Barat on wheels', 'Barat on wheels'),
         ('Other', 'Other'),
     ]
+
     event_name = models.CharField(
         max_length=100,
         blank=True,
